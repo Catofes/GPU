67c53bc88736043529affb5c17de55bffe3a7ec5
Catofes <i@catofes.com>
2018-03-18 16:14:18 +0800

    Update pic2

--- analyse/pic2.py
+++ analyse/pic2.py
@@ -2,11 +2,11 @@ import ROOT
 
 data = '''
 1000	2000	5000	10000	20000	50000	100000	200000	500000	1000000	2000000	5000000	10000000	20000000	50000000	100000000	200000000
-201978.222	390286.6296	1002573.632	1939073.078	3861769.681	9642033.291	19337215.41	38571405.16	96346875.83	192520007.1							
-136145.7634	271690.1999	679476.3514	1350056.82	2692366.42	6707720.963	13536351.74	26822811.38	67501444.75	135325862.6							
-675838.3719	689284.174	682012.0855	692447.0363	708162.8283	707609.1143	704604.822	715761.3075	794323.4858	1235948.57	1769308.889	3952166.474	8150731.445	14985905.01	37768743.25	80963736.11	151628833.5
-665486.6821	677809.0957	669579.8981	676525.0607	689790.3648	689647.3321	686758.718	692329.744	718036.9363	984642.0624	1381392.069	2981092.759	6207988.377	11076530.44	27949132.55	61057992.85	111894993.5
-'''
+237347.5352	477346.2078	1180622.82	2359769.724	4743672.789	11642909.31	23236435.41	46363395.35	116394465.5	233281090.6							
+164701.3639	331171.279	820183.7044	1646363.02	3286950.909	8211757.316	16387862.79	32788333.87	81793799.9	163761205.2							
+677170.8065	670512.9781	681801.5873	671672.3518	689400.1411	681922.5521	677230.1758	698317.747	708469.6118	1093248.443	1855289.817	4197851.292	8654051.257	16018435.87	40395855.12	90392631.05	162421769.8
+676947.0323	670534.8703	679443.6121	672247.566	687328.6633	678100.8933	677834.0261	694619.8749	694064.549	908787.2109	1479214.547	3238553.496	6724302.937	12122568.49	30600717.97	70503813.12	122765455.8
+196684.2483	196999.2088	207095.3594	194580.6936	205486.3777	203428.6599	205320.7774	291440.0003	411663.5315	704100.7427	1273253.392	3032314.615	6520269.413	11913798.28	30392659.03	70299273.66	122552100.6'''
 
 lines = data.strip().split("\n")
 lines = [line.strip() for line in lines]
@@ -21,6 +21,9 @@ GPUGauss = lines[3]
 GPUGauss = [float(v) for v in GPUGauss]
 GPURiemann = lines[4]
 GPURiemann = [float(v) for v in GPURiemann]
+GPURiemannTuned = lines[5]
+GPURiemannTuned = [float(v) for v in GPURiemannTuned]
+
 
 tCanvas1 = ROOT.TCanvas("c1")
 tCanvas1.cd()
@@ -89,4 +92,49 @@ tLegend.Draw()
 tCanvas1.SetLogy()
 tCanvas1.SetLogx()
 tCanvas1.SetTitle("")
+
+
+tCanvas2 = ROOT.TCanvas("c2")
+tCanvas2.cd()
+tTmpHist2 = ROOT.TH2F("tmp2", "", 100, 500, 300000000, 100, 100000, 200000000)
+tTmpHist2.Draw()
+tTmpHist2.SetStats(False)
+tTmpHist2.GetXaxis().SetTitle("Bins")
+tTmpHist2.GetXaxis().CenterTitle()
+tTmpHist2.GetYaxis().SetTitle("Time / ns")
+tTmpHist2.GetYaxis().CenterTitle()
+
+graphGPURiemann2 = ROOT.TGraph(len(GPURiemann))
+for i in range(0, len(GPURiemann)):
+    graphGPURiemann2.SetPoint(i, X[i], GPURiemann[i])
+graphGPURiemann2.SetTitle("GPU Riemann")
+graphGPURiemann2.SetLineColor(8)
+graphGPURiemann2.SetLineWidth(3)
+graphGPURiemann2.SetMarkerStyle(20)
+graphGPURiemann2.SetMarkerColor(8)
+graphGPURiemann2.SetMarkerSize(1)
+graphGPURiemann2.Draw("PL")
+
+graphGPURiemannTuned = ROOT.TGraph(len(GPURiemannTuned))
+for i in range(0, len(GPURiemannTuned)):
+    graphGPURiemannTuned.SetPoint(i, X[i], GPURiemannTuned[i])
+graphGPURiemannTuned.SetTitle("GPU Riemann Tuned")
+graphGPURiemannTuned.SetLineColor(49)
+graphGPURiemannTuned.SetLineWidth(3)
+graphGPURiemannTuned.SetMarkerStyle(21)
+graphGPURiemannTuned.SetMarkerColor(49)
+graphGPURiemannTuned.SetMarkerSize(1)
+graphGPURiemannTuned.Draw("PL")
+
+
+tLegend2 = ROOT.TLegend(0.2, 0.6, 0.5, 0.8)
+tLegend2.SetFillColor(0)
+tLegend2.AddEntry(graphGPURiemann2, "GPU Riemann", "PL")
+tLegend2.AddEntry(graphGPURiemannTuned, "GPU Riemann Tuned", "PL")
+tLegend2.Draw()
+
+tCanvas2.SetLogy()
+tCanvas2.SetLogx()
+tCanvas2.SetTitle("")
+
 input()
